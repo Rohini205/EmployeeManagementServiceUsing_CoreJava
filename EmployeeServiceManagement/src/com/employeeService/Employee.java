1309572694b520dfdecf10782705abe6dc80d4aa
unknown <rohinitibile25@gmail.com>
2023-12-06 14:11:01 +0530

    EmployeeManagementSystem

---
+++ EmployeeServiceManagement/src/com/employeeService/Employee.java
@@ -0,0 +1,69 @@
+package com.employeeService;
+
+public class Employee {
+	
+	private int id;
+	private String name;
+	private int age;
+	private String designasation;
+	private String department;
+	private double salary;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDesignasation() {
+		return designasation;
+	}
+	public void setDesignasation(String designasation) {
+		this.designasation = designasation;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public Employee(int id, String name, int age, String designasation, String department, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.designasation = designasation;
+		this.department = department;
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", designasation=" + designasation
+				+ ", department=" + department + ", salary=" + salary + "]";
+	}
+	
+	
+	
+	
+
+}

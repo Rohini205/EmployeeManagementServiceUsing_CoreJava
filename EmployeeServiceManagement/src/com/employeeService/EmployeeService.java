8c1b7e92680b68437ff81fb3641605d702c17dde
Rohini Hari Tibile <84588473+Rohini205@users.noreply.github.com>
2023-12-06 14:31:09 +0530

    Update EmployeeService.java

--- EmployeeServiceManagement/src/com/employeeService/EmployeeService.java
+++ EmployeeServiceManagement/src/com/employeeService/EmployeeService.java
@@ -101,6 +101,25 @@ public class EmployeeService {
 	    }
 	//Add employee
     public void addEmp() {
+	   System.out.println("Enter id:");
+		id=sc.nextInt();
+		System.out.println("Enter name");
+		name=sc.next();
+		System.out.println("Enter age");
+		age=sc.nextInt();
+		System.out.println("enter Desiganation");
+		desiganation=sc.next();
+		System.out.println("Enter Department");
+		department=sc.next();
+		System.out.println("Enter sal");
+		sc.nextDouble();
+		
+		Employee emp=new Employee(id, name, age, desiganation, department, sal);
+		
+		empset.add(emp);
+		System.out.println(emp);
+		System.out.println("Employtee addeed successsfully");
+		
 	}
 	
 }
